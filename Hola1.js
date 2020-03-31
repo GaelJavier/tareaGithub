d5db9300fce66584f839f49f2341f4ae7c1527dc
GaelJavier <pacheco.gaellopez@hotmail.com>
2020-03-31 16:43:43 -0600

    firts commit

---
+++ Hola1.js
@@ -0,0 +1,16 @@
+var http = require("http");
+
+var hola = function(solicitud,respuesta){
+	console.log("Hola Mundo");
+    console.log("       *");
+	console.log("      **");
+    console.log("     ****");
+	console.log("    ******");
+    console.log("   ********");
+    console.log("  ***********");
+
+}
+
+var servidor = http.createServer(hola);
+
+servidor.listen(8080)
